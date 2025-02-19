f650442bbc73437cea5784634a089dfa8b20f61e
SimonTan1 <simonsays248@gmail.com>
2025-02-18 19:47:45 -0500

    changed animal class functions

--- Animal.py
+++ Animal.py
@@ -1,7 +1,24 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    species1 = "Homo Sapiens"
+    
+    def __init__(self, name):
+        self.__name = name
+        print("hello, i am", self.__name)
 
     def talk(self):
         print("hi")
-
+        print("I can talk")
+     
+    def weight(self, w):
+        self.weight = w
+        if self.weight > 100:
+            print("i am heavier than 100 lbs")
+        else:
+            print("i am under 100 pounds")
+            
+    def species(self):
+        print(self.species1)
+ 
+            
+    
+    
